8bda38fda477842796261037b3b225f057daf093
Maksim <kodermax@gmail.com>
2016-05-17 23:11:39 +0300

    change home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,15 @@ import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
+    <h1>Заявки</h1>
     <IndexLink to='/' activeClassName={classes.activeRoute}>
-      Home
+      Главная
     </IndexLink>
     {' · '}
     <Link to='/counter' activeClassName={classes.activeRoute}>
       Counter
     </Link>
   </div>
-)
+);
 
 export default Header
