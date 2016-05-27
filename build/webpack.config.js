ee7f8e3c650668726d861d5245e3fce735fe64e0
Maksim Karpychev <kodermax@gmail.com>
2016-05-27 12:14:37 +0300

    fix for heroku

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -63,7 +63,7 @@ if (__DEV__) {
   webpackConfig.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  )
+  );
 } else if (__PROD__) {
   debug('Enable plugins for production (OccurenceOrder, Dedupe & UglifyJS).');
   webpackConfig.plugins.push(
@@ -76,7 +76,7 @@ if (__DEV__) {
         warnings: false
       }
     })
-  )
+  );
 }
 
 // Don't split bundles during testing, since we only want import one bundle
@@ -85,7 +85,7 @@ if (!__TEST__) {
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor']
     })
-  )
+  );
 }
 
 // ------------------------------------
@@ -143,7 +143,7 @@ webpackConfig.module.loaders = [{
 // ------------------------------------
 // We use cssnano with the postcss loader, so we tell
 // css-loader not to duplicate minimization.
-const BASE_CSS_LOADER = 'css?sourceMap&-minimize'
+const BASE_CSS_LOADER = 'css?sourceMap&-minimize';
 
 // Add any packge names here whose styles need to be treated as CSS modules.
 // These paths will be combined into a single regex.
@@ -155,11 +155,11 @@ const PATHS_TO_TREAT_AS_CSS_MODULES = [
 if (config.compiler_css_modules) {
   PATHS_TO_TREAT_AS_CSS_MODULES.push(
     paths.client().replace(/[\^\$\.\*\+\-\?\=\!\:\|\\\/\(\)\[\]\{\}\,]/g, '\\$&') // eslint-disable-line
-  )
+  );
 }
 
 const isUsingCSSModules = !!PATHS_TO_TREAT_AS_CSS_MODULES.length
-const cssModulesRegex = new RegExp(`(${PATHS_TO_TREAT_AS_CSS_MODULES.join('|')})`)
+const cssModulesRegex = new RegExp(`(${PATHS_TO_TREAT_AS_CSS_MODULES.join('|')})`);
 
 // Loaders for styles that need to be treated as CSS modules.
 if (isUsingCSSModules) {
@@ -189,11 +189,11 @@ if (isUsingCSSModules) {
       cssModulesLoader,
       'postcss'
     ]
-  })
+  });
 }
 
 // Loaders for files that should not be treated as CSS modules.
-const excludeCSSModules = isUsingCSSModules ? cssModulesRegex : false
+const excludeCSSModules = isUsingCSSModules ? cssModulesRegex : false;
 webpackConfig.module.loaders.push({
   test: /\.scss$/,
   exclude: excludeCSSModules,
@@ -259,20 +259,20 @@ webpackConfig.module.loaders.push(
 // need to use the extractTextPlugin to fix this issue:
 // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
 if (!__DEV__) {
-  debug('Apply ExtractTextPlugin to CSS loaders.')
+  debug('Apply ExtractTextPlugin to CSS loaders.');
   webpackConfig.module.loaders.filter((loader) =>
     loader.loaders && loader.loaders.find((name) => /css/.test(name.split('?')[0]))
   ).forEach((loader) => {
-    const [first, ...rest] = loader.loaders
-    loader.loader = ExtractTextPlugin.extract(first, rest.join('!'))
-    Reflect.deleteProperty(loader, 'loaders')
+    const [first, ...rest] = loader.loaders;
+    loader.loader = ExtractTextPlugin.extract(first, rest.join('!'));
+    Reflect.deleteProperty(loader, 'loaders');
   });
 
   webpackConfig.plugins.push(
     new ExtractTextPlugin('[name].[contenthash].css', {
       allChunks: true
     })
-  )
+  );
 }
 
-export default webpackConfig
+export default webpackConfig;
