a0c8084d0d2f67266c444301715ce2c48168ead1
Maksim Karpychev <kodermax@gmail.com>
2016-05-27 13:19:24 +0300

    fix host

--- bin/server.js
+++ bin/server.js
@@ -4,11 +4,8 @@ import _debug from 'debug';
 
 const debug = _debug('app:bin:server');
 const port = config.server_port;
-const host = config.server_host;
-if (config.env !== 'development') {
-  server.listen(port, '0.0.0.0');
-} else {
-  server.listen(port, host);
-}
+const host = config.env !== 'development' ? '0.0.0.0' : config.server_host;
+server.listen(port, host);
+
 debug(`Server is now running at http://${host}:${port}.`);
 debug(`Server accessible via localhost:${port} if you are using the project defaults.`);
