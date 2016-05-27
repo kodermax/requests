c6d13861080114e87a5d188b992bb73df951f300
Maksim Karpychev <kodermax@gmail.com>
2016-05-27 13:00:51 +0300

    fix for heroku

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,14 @@
-import config from '../config'
-import server from '../server/main'
-import _debug from 'debug'
+import config from '../config';
+import server from '../server/main';
+import _debug from 'debug';
 
-const debug = _debug('app:bin:server')
-const port = config.server_port
-const host = config.server_host
-
-server.listen(port)
-debug(`Server is now running at http://${host}:${port}.`)
-debug(`Server accessible via localhost:${port} if you are using the project defaults.`)
+const debug = _debug('app:bin:server');
+const port = config.server_port;
+const host = config.server_host;
+if (config.env !== 'development') {
+  server.listen(port);
+} else {
+  server.listen(port, host);
+}
+debug(`Server is now running at http://${host}:${port}.`);
+debug(`Server accessible via localhost:${port} if you are using the project defaults.`);
