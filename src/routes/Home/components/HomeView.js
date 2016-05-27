902a3e0fb28dfbf6cad209d4aec6ce7f41c34726
Maksim Karpychev <kodermax@gmail.com>
2016-05-27 09:27:57 +0300

    fix eslint

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -9,17 +9,19 @@ export const HomeView = () => (
     <h4>Добро пожаловать!</h4>
     <div className={cx('g-cols')}>
       <div className={cx('one-third')}>
-        <div className={cx('w-iconbox', 'iconpos_top','size_medium', 'style_circle', 'color_light')}>
-          <div className={cx('w-iconbox-icon')}><i class="mdfi_hardware_phonelink"></i></div>
+        <div className={cx('w-iconbox', 'iconpos_top', 'size_medium', 'style_circle', 'color_light')}>
+          <div className={cx('w-iconbox-icon')}><i className='mdfi_hardware_phonelink'></i></div>
           <h4 className={cx('w-iconbox-title')}>Web Development</h4>
-          <div className={cx('w-iconbox-text')}>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
+          <div className={cx('w-iconbox-text')}>
+            Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
           </div>
         </div>
-        <div className={cx('w-separator', 'type_invisible', 'size_medium', 'thick_1', 'style_solid', 'color_border', 'cont_none')}>
+        <div className={cx('w-separator', 'type_invisible', 'size_medium', 'thick_1', 'style_solid', 'color_border',
+          'cont_none')}>
           <span className={cx('w-separator-h')}> </span>
         </div>
         <div className={cx('w-iconbox', 'iconpos_top', 'size_medium', 'style_circle', 'color_light')}>
-          <div className={cx('w-iconbox-icon')}><i class="mdfi_file_cloud_upload"></i></div>
+          <div className={cx('w-iconbox-icon')}><i className='mdfi_file_cloud_upload'></i></div>
           <h4 className={cx('w-iconbox-title')}>Cloud Hosting</h4>
           <div className={cx('w-iconbox-text')}>
             Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
@@ -27,17 +29,19 @@ export const HomeView = () => (
         </div>
       </div>
       <div className={cx('one-third')}>
-        <div className={cx('w-iconbox', 'iconpos_top','size_medium', 'style_circle', 'color_light')}>
-          <div className={cx('w-iconbox-icon')}><i class="mdfi_hardware_phonelink"></i></div>
+        <div className={cx('w-iconbox', 'iconpos_top', 'size_medium', 'style_circle', 'color_light')}>
+          <div className={cx('w-iconbox-icon')}><i className='mdfi_hardware_phonelink'></i></div>
           <h4 className={cx('w-iconbox-title')}>Web Development</h4>
-          <div className={cx('w-iconbox-text')}>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
+          <div className={cx('w-iconbox-text')}>
+            Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
           </div>
         </div>
-        <div className={cx('w-separator', 'type_invisible', 'size_medium', 'thick_1', 'style_solid', 'color_border', 'cont_none')}>
+        <div className={cx('w-separator', 'type_invisible', 'size_medium', 'thick_1', 'style_solid', 'color_border',
+          'cont_none')}>
           <span className={cx('w-separator-h')}> </span>
         </div>
         <div className={cx('w-iconbox', 'iconpos_top', 'size_medium', 'style_circle', 'color_light')}>
-          <div className={cx('w-iconbox-icon')}><i class="mdfi_file_cloud_upload"></i></div>
+          <div className={cx('w-iconbox-icon')}><i className='mdfi_file_cloud_upload'></i></div>
           <h4 className={cx('w-iconbox-title')}>Cloud Hosting</h4>
           <div className={cx('w-iconbox-text')}>
             Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
@@ -45,17 +49,19 @@ export const HomeView = () => (
         </div>
       </div>
       <div className={cx('one-third')}>
-        <div className={cx('w-iconbox', 'iconpos_top','size_medium', 'style_circle', 'color_light')}>
-          <div className={cx('w-iconbox-icon')}><i class="mdfi_hardware_phonelink"></i></div>
+        <div className={cx('w-iconbox', 'iconpos_top', 'size_medium', 'style_circle', 'color_light')}>
+          <div className={cx('w-iconbox-icon')}><i className='mdfi_hardware_phonelink'></i></div>
           <h4 className={cx('w-iconbox-title')}>Web Development</h4>
-          <div className={cx('w-iconbox-text')}>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
+          <div className={cx('w-iconbox-text')}>
+            Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
           </div>
         </div>
-        <div className={cx('w-separator', 'type_invisible', 'size_medium', 'thick_1', 'style_solid', 'color_border', 'cont_none')}>
+        <div className={cx('w-separator', 'type_invisible', 'size_medium', 'thick_1', 'style_solid', 'color_border',
+          'cont_none')}>
           <span className={cx('w-separator-h')}> </span>
         </div>
         <div className={cx('w-iconbox', 'iconpos_top', 'size_medium', 'style_circle', 'color_light')}>
-          <div className={cx('w-iconbox-icon')}><i class="mdfi_file_cloud_upload"></i></div>
+          <div className={cx('w-iconbox-icon')}><i className='mdfi_file_cloud_upload'></i></div>
           <h4 className={cx('w-iconbox-title')}>Cloud Hosting</h4>
           <div className={cx('w-iconbox-text')}>
             Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et.
