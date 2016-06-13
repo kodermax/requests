18108e2840b2d045e4fa8d9c3f8583dbbe280d59
Maksim Karpychev <kodermax@gmail.com>
2016-06-13 19:08:28 +0300

    fix eslint

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,16 +4,16 @@ import { Button } from 'react-biz/lib/button';
 import AppBar from 'react-biz/lib/app_bar';
 import LoadingBar from '../Bar/LoadingBar';
 
-export const Header = () =>  (
-      <div>
-        <h1>Заявки</h1>
-        <AppBar flat>
-          <Link to='/list'>
-            <Button label='Мои заявки' raised primary />
-          </Link>
-        </AppBar>
-        <LoadingBar />
-      </div>
+export const Header = () => (
+  <div>
+    <h1>Заявки</h1>
+    <AppBar flat>
+      <Link to='/list'>
+        <Button label='Мои заявки' raised primary/>
+      </Link>
+    </AppBar>
+    <LoadingBar />
+  </div>
 );
 
 export default Header;
