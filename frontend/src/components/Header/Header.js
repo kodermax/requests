383338c2036ed8d95385ab25a7060165d2371d4a
Maksim Karpychev <kodermax@gmail.com>
2016-06-14 23:43:57 +0300

    делаем app bar

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { Component} from 'react';
 import { Button } from 'react-biz/lib/button';
 import AppBar from 'react-biz/lib/app_bar';
+import Badge from 'react-biz/lib/badge';
 import LoadingBar from '../Bar/LoadingBar';
 import theme from './theme.scss';
 
-export const Header = () => (
-  <div>
-    <h1>Заявки</h1>
-    <AppBar flat theme={theme}>
-      <Link to='/list'>
-        <Button label='Мои заявки' raised primary />
-      </Link>
-    </AppBar>
-    <LoadingBar />
-  </div>
-);
+export default class Header extends Component {
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  };
+  toHome = (e) => {
+    e.preventDefault();
+    this.context.router.push('/');
+  };
+  toList = (e) => {
+    e.preventDefault();
+    this.context.router.push('/list');
+  }
 
+  render () {
+    return (
+      <div>
+        <h1>Заявки</h1>
+        <AppBar flat theme={theme}>
+          <Button label='Мои заявки' raised onMouseUp={::this.toList} />
+          <Badge theme={theme} badgeContent={10} secondary badgeStyle={{top: 12, right: 12}}>
+            <Button label='Исполняю' raised />
+          </Badge>
+          <Button label='Создать' raised primary onMouseUp={::this.toHome} />
+        </AppBar>
+        <LoadingBar />
+      </div>
+    );
+  }
+}
 export default Header;
