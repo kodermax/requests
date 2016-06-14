512bf084de58c2d94b5e7c8d943e687269e3f1f1
Maksim Karpychev <kodermax@gmail.com>
2016-06-14 23:05:50 +0300

    улучшение

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router';
 import { Button } from 'react-biz/lib/button';
 import AppBar from 'react-biz/lib/app_bar';
 import LoadingBar from '../Bar/LoadingBar';
+import theme from './theme.scss';
 
 export const Header = () => (
   <div>
     <h1>Заявки</h1>
-    <AppBar flat>
+    <AppBar flat theme={theme}>
       <Link to='/list'>
         <Button label='Мои заявки' raised primary />
       </Link>
