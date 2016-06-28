6affdd3617246c5b0d4ff9e3b5dae1f9cf345d44
Maksim Karpychev <kodermax@gmail.com>
2016-06-28 23:48:15 +0300

    jwt middleware

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -3,6 +3,7 @@ import logger from 'koa-logger';
 import convert from 'koa-convert';
 import cors from 'koa-cors';
 import bodyParser from 'koa-bodyparser';
+import jwtValidate from './jwt';
 
 export default () => compose([
   logger(),
@@ -12,4 +13,5 @@ export default () => compose([
       json: ['application/json'],
     },
   }),
+  jwtValidate({ secret: 'FNipuDZhwdnkBK9AzpJKi0TGbXmMWk01', jwtOpts: { algorithms:['HS512'] } }),
 ]);
---
+++ backend/middleware/jwt.js
@@ -0,0 +1,16 @@
+import JWT from 'jsonwebtoken';
+
+module.exports = jwtValidate;
+
+function jwtValidate(options) {
+  return async function (ctx, next) {
+    if (ctx && ctx.header && ctx.header.authorization) {
+      const authComponents = ctx.header.authorization.split(' ');
+      if (authComponents.length === 2 && authComponents[0] === 'Bearer') {
+        const user = await JWT.verify(authComponents[1], options.secret, options.jwtOpts);
+        console.log(user);
+      }
+    }
+    return next();
+  };
+}
