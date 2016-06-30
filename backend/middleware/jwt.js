d73cbb076ee17994094d860f2912a17c990477c2
Maksim Karpychev <kodermax@gmail.com>
2016-06-30 22:29:02 +0300

    fix eslint

--- backend/middleware/jwt.js
+++ backend/middleware/jwt.js
@@ -3,7 +3,7 @@ import JWT from 'jsonwebtoken';
 module.exports = jwtValidate;
 
 function jwtValidate(options) {
-  return async function (ctx, next) {
+  return async function validate(ctx, next) {
     if (ctx && ctx.header && ctx.header.authorization) {
       const authComponents = ctx.header.authorization.split(' ');
       if (authComponents.length === 2 && authComponents[0] === 'Bearer') {
