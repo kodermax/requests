31d83d33eef92d5826b211c4a35df8efbdad712e
Maksim Karpychev <kodermax@gmail.com>
2016-06-30 23:39:02 +0300

    model Request

--- backend/models/Request.js
+++ backend/models/Request.js
@@ -1,37 +1,12 @@
 import mongoose from 'mongoose';
+import User from './User';
+const Schema = mongoose.Schema;
 
-class Request {
-  constructor(db) {
-    this.db = db || null;
-    this.request = {};
-  }
-
-  async all() {
-    return await r.table('requests').run(this.db)
-      .then(cursor => cursor.toArray())
-      .then(result => JSON.stringify(result));
-  }
-
-  async create(request) {
-    request.created = new Date().toJSON();
-    request.changed = new Date().toJSON();
-    return await r.table('requests').insert(request, { returnChanges: true }).run(this.db);
-  }
-
-  async fields(code) {
-    const result = await r.table('requests_cats').filter(r.row('items').contains((item) => {
-      return item('code').eq(code);
-    })).pluck(
-      {
-        items:
-        {
-          fields: true,
-        },
-      })
-      .run(this.db)
-      .then(cursor => cursor.toArray());
-    return result[0].items[0].fields;
-  }
-}
-
-module.exports = Request;
+const requestSchema = new Schema({
+  author: User,
+  changed: Date,
+  created: Date,
+  fields: Schema.Types.Mixed,
+  title: String,
+});
+export default mongoose.model('Request', requestSchema);
