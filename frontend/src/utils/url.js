a4be4b0d4d89804a54b255fd1aef651dbb72d461
Maksim Karpychev <kodermax@gmail.com>
2016-07-20 22:05:40 +0300

    fix url

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,7 +1,10 @@
-export default function encodeQueryData(data) {
+let Url = {};
+Url.encodeQueryData = (data) => {
   var ret = [];
   for (var d in data) {
     ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
   }
   return ret.join('&');
-}
+};
+
+module.exports = Url;
