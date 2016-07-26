112782a1d5c160217ed7e7cb0c5a17b0358a722e
Maksim Karpychev <kodermax@gmail.com>
2016-07-26 11:20:37 +0300

    сортировка заявок

--- backend/controllers/requests.js
+++ backend/controllers/requests.js
@@ -17,12 +17,11 @@ class Requests {
     if (this.ctx.query.conditions) {
       conditions = JSON.parse(this.ctx.query.conditions);
     }
-    console.log(conditions);
-    const result = await Request.find(conditions).populate('author');
+    const result = await Request.find(conditions).populate('author').sort({ requestId: 'desc' });
     if (result) {
       this.respond.body = result;
     } else {
-      this.respond.body = { message: 'Ошибка' };
+      this.respond.body = {message: 'Ошибка'};
     }
     this.respond.status = 200;
   }
@@ -33,23 +32,23 @@ class Requests {
 
   async create() {
     await this.parseRequest();
-    const user = await User.findOne({btxId: this.ctx.passport.user.Id});
+    const user = await User.findOne({ btxId: this.ctx.passport.user.Id });
     if (user) {
       this.request.author = user._id;
     }
     this.request.messages = 0;
     this.request.status = {
       code: 'wait',
-      title: 'Ожидает исполнителя'
+      title: 'Ожидает исполнителя',
     };
     const request = new Request(this.request);
     const result = await request.save();
     if (result) {
       this.respond.status = 201;
-      this.respond.body = {id: result.id};
+      this.respond.body = { id: result.id };
     } else {
       this.respond.status = 200;
-      this.respond.body = {message: 'Ошибка при добавлении заявки'};
+      this.respond.body = { message: 'Ошибка при добавлении заявки' };
     }
   }
 
@@ -62,9 +61,9 @@ class Requests {
   response() {
     this.ctx.status = this.respond.status || 500;
     this.ctx.body = this.respond.body || {
-        code: this.ctx.status,
-        data: {error: 'An internal error has occured'},
-      };
+      code: this.ctx.status,
+      data: { error: 'An internal error has occured' },
+    };
     this.ctx.type = 'application/json';
   }
 }
