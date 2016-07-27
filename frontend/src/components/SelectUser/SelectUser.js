0e035195a3e30d8947d47159a538a3042846d756
Maksim Karpychev <kodermax@gmail.com>
2016-07-27 11:52:21 +0300

    fix eslint

--- frontend/src/components/SelectUser/SelectUser.js
+++ frontend/src/components/SelectUser/SelectUser.js
@@ -1,41 +1,38 @@
 import React from 'react';
 import Autosuggest from 'react-biz/lib/autosuggest';
 import Avatar from 'react-biz/lib/avatar';
+import FontIcon from 'react-biz/lib/font_icon';
 import Highlighter from 'react-highlight-words/dist/main';
 import style from './SelectUser.scss';
 
-const PersonIcon = (props) => (
-  <svg {...props} fill="#000000" viewBox="0 0 24 36" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
-    <path d="M0 0h24v24H0z" fill="none"/>
-  </svg>
-);
 export default class SelectUser extends React.Component {
   static propTypes = {
     error: React.PropTypes.string,
     label: React.PropTypes.string,
     onChange: React.PropTypes.func,
-    value: React.PropTypes.oneOfType([React.PropTypes.object, React.PropTypes.string])
+    value: React.PropTypes.oneOfType([React.PropTypes.object, React.PropTypes.string]),
   };
   state = {
     countries: '',
     query: '',
     user: this.props.value,
-    users: []
+    users: [],
   };
 
-  componentDidMount () {
+  componentDidMount() {
     this.fetchData();
   }
-  componentWillReceiveProps (nextProps) {
+
+  componentWillReceiveProps(nextProps) {
     this.setState({user: nextProps.value});
   }
+
   fetchData = (q) => {
     if (!q) {
       q = '';
     }
     let token = localStorage.getItem('userToken') || null;
-    let filter = { '$or': [ { name: q }, { lastName: q } ] };
+    let filter = {'$or': [{name: q}, {lastName: q}]};
 
     return fetch(`http://10.1.1.219:3001/api/users?conditions=${JSON.stringify(filter)}`,
       {
@@ -82,7 +79,7 @@ export default class SelectUser extends React.Component {
     return (
       <div style={containerStyle}>
         {item.img ? <Avatar><img src={item.img} /></Avatar> :
-          <Avatar style={avatarStyle} icon={<PersonIcon height="60" width="60" />} />}
+          <Avatar style={avatarStyle} icon={<FontIcon value="person" />} />}
         <div style={contentStyle}>
           <Highlighter highlightClassName={style.highlightRed}
             searchWords={this.state.query.trim().length > 0 ? this.state.query.split(' ') : []}
@@ -93,7 +90,8 @@ export default class SelectUser extends React.Component {
       </div>
     );
   };
-  render () {
+
+  render() {
     let label = this.props.label ? this.props.label : 'Выбор сотрудника';
     return (
       <Autosuggest
